2ba77940aef35f51d53985bb8dd4d65c3a577b06
Wilver Emilio Xiá Ixcot <wilb123@umes.edu.gt>
2025-01-28 18:08:50 -0600

    Ejemplo de validación de datos

--- Ejemplo_Pydantic/main.py
+++ Ejemplo_Pydantic/main.py
@@ -1,19 +1,41 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, ValidationError
 
 class User(BaseModel):
     username: str
     password: str
+    repeat_password: str
     email: str
     age: int
     
     class Config:
         orm_mode = True
-    
-user = User(
-    username="johndoe",
-    password="secret",
-    email="hola@gmail.com",
-    age=25
-)
+        
+    @validator("username")    
+    def username_validation(cls, username: str):
+        if len(username) < 4:
+            raise ValueError("La longitud del username debe ser mayor a 4")
+        
+        if len(username) > 20:
+            raise ValueError("La longitud del username debe ser menor a 20")
+        
+        return username
 
-print(user)
+    @validator("repeat_password")
+    def password_validation(cls, repeat_password, values):
+        if 'password' in values and repeat_password != values['password']:
+            raise ValueError("Las contraseñas no coinciden")
+        
+        return repeat_password
+
+try: 
+    user = User(
+        username="Wilson",
+        password="secret",
+        repeat_password="hoal",
+        email="hola@gmail.com",
+        age=25
+    )
+
+    print(user)
+except ValueError as e:
+    print(e.json())
