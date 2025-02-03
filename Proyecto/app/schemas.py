ec09f55f31f4509762fe5fa2da89627cb2743a73
Wilver Emilio Xiá Ixcot <wilb123@umes.edu.gt>
2025-02-02 19:36:24 -0600

    arreglando el codigo para pydantic v2

--- Proyecto/app/schemas.py
+++ Proyecto/app/schemas.py
@@ -1,70 +1,58 @@
-from pydantic import BaseModel, validator
-from pydantic.utils import GetterDict
+from pydantic import BaseModel, field_validator
 from typing import Any
 from peewee import ModelSelect
+from playhouse.shortcuts import model_to_dict
 
-class PeeweeGetterDict(GetterDict):
-    def get(self, key: Any, default: Any = None):
-        res = getattr(self._obj, key, default)
-        if isinstance(res, ModelSelect):
-            return list(res)
-        return res
+class ResponseModel(BaseModel):
+    class Config:
+        from_attributes = True  
+    
+    @classmethod
+    def from_peewee(cls, peewee_instance):
+        """ Convierte un objeto Peewee en un modelo Pydantic """
+        return cls(**model_to_dict(peewee_instance, backrefs=True))  
 
 class UserRequestModel(BaseModel):
     username: str
     password: str
     
-    class Config: 
-         from_attributes = True  #Para que se pueda crear un objeto de la clase User a partir de un diccionario
-         
-    @validator('username')
+    @field_validator('username')
+    @classmethod
     def username_validator(cls, username):
-        if len(username) < 3 or len(username) > 50:
-            raise ValueError('El nombre de usuario debe tener al menos 3 caracteres')
-        
+        if not (3 <= len(username) <= 50):
+            raise ValueError('El nombre de usuario debe tener entre 3 y 50 caracteres')
         return username
-    
-class ResponseModel(BaseModel):
-    class Config:
-        orm_mode = True
-        getter_dict = PeeweeGetterDict
-    
+
+
 class UserResponseModel(ResponseModel):
-    id : int
+    id: int
     username: str
-    
-    
-#------ MOVIE -----
+
 
 class MovieResponseModel(ResponseModel):
     id: int
     title: str
-    
-#------ RESEÑAS ------       
-class ReviewValidator():
-     @validator('score')
-     def score_validator(cls, score):
-        if score < 1 or score > 5:
+
+class ReviewValidator:
+    @field_validator('score')
+    @classmethod
+    def score_validator(cls, score):
+        if not (1 <= score <= 5):
             raise ValueError('La puntuación debe ser entre 1 y 5')
-        
         return score
-            
-        
+
 class ReviewRequestModel(BaseModel, ReviewValidator): 
     movie_id: int
     user_id: int
     review: str
     score: int
-    
-    
+
 class ReviewResponseModel(ResponseModel):
     id: int
     movie: MovieResponseModel
     review: str
     score: int
-    
+
 class ReviewRequestPutModel(BaseModel, ReviewValidator):
     review: str
-    score: int   
-    
-    
+    score: int
