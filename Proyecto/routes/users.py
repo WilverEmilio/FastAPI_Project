5d73bb119f9de9f6c11701585f6e499009c079cf
Wilver Emilio Xiá Ixcot <wilb123@umes.edu.gt>
2025-02-02 21:17:34 -0600

    Agregando un login

--- Proyecto/routes/users.py
+++ Proyecto/routes/users.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException, APIRouter
+from fastapi.security import HTTPBasicCredentials
 from app.database import User
 from app.schemas import UserRequestModel, UserResponseModel
-
 
 router = APIRouter(prefix='/users', tags=['Users'])
 
@@ -19,3 +19,17 @@ async def create_user(user: UserRequestModel):
         )
     
     return UserResponseModel(id = user.id, username = user.username)
+
+
+@router.post('/login', response_model=UserResponseModel)
+async def login(credentials: HTTPBasicCredentials):
+    
+    user = User.select().where(User.username == credentials.username).first()
+    
+    if user is None:
+        raise HTTPException(status_code=404, detail='El usuario no existe', headers={'X-Error': 'El usuario no existe'})
+    
+    if user.password != User.create_password(credentials.password):
+        raise HTTPException(status_code=401, detail='La contraseña es incorrecta', headers={'X-Error': 'La contraseña es incorrecta'})
+    
+    return user
